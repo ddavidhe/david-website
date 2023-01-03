a3925eafc8c3e10d2ea99212cb06d1821f8759d0
xxXeeno <davidhe.3612@gmail.com>
2023-01-02 19:44:42 -0500

    ditched scss, now written in the modern css :)

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,40 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
+import './index.css';
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = [' ', 'D', 'a', 'v', 'i', 'd', ' ', 'H', 'e', ',']
+    const subArray = ['U', 'W', ' ', 'S', 't', 'u', 'd', 'e', 'n', 't']
+
+    useEffect(() => {
+      setTimeout(() => {
+        return setLetterClass('text-animate-hover')
+      }, 4000)
+    }, []);
+    
 
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br/> I'm David He
+                <h1> 
+                <span className= {letterClass}>H</span>
+                <span className= {`${letterClass} _12`}>i,</span>                    
+                    <br/>
+                <span className= {`${letterClass} _13`}>I</span>
+                <span className= {`${letterClass} _14`}>'m</span>
+
+
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray} 
+                index = {15}/>
                 <br/>
-                UW Student
+                <AnimatedLetters letterClass={letterClass}
+                strArray={subArray}
+                index = {25}/>
                 </h1>
-                <h2> gacha addict, aspiring programmer, sock matcher</h2>
+                <h2> gacha addict, aspiring programmer</h2>
                 <Link to="/contact" className="flat-button"> CONTACT ME </Link>
             </div>
 
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,5 +1,5 @@
 import { Link, NavLink } from 'react-router-dom'
-import './index.scss'
+import './index.css'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
