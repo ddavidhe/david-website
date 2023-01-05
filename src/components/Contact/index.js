a9314f0e6daf7ac59176f248f62cd3385b3841c3
xxXeeno <davidhe.3612@gmail.com>
2023-01-04 20:51:21 -0500

    email is functioning perfectly. contact :D

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,8 +1,32 @@
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(
+            "service_la2ze73",
+            "template_httyxhi",
+            form.current,
+            "aIPq14S5ThxGkDvLS"
+            )
+            .then(
+                (result) => {
+                    console.log("sent!");
+                    alert('Email sent successfully!')
+                    e.target.reset();
+                },
+                (error) => {
+                    alert('Email failed to send, maybe try again? :(')
+                    console.log("failed :(");
+                }
+            );
+    };
 
     const [letterClass, setLetterClass] = useState('text-animate')
 
@@ -11,6 +35,9 @@ const Contact = () => {
           return setLetterClass('text-animate-hover')
         }, 3000)
       }, []);
+
+    
+
 
     return (
     <>
@@ -26,13 +53,13 @@ const Contact = () => {
                     I am interested in getting a job. Please message me for job opps :3
                 </p>
                 <div className='contact-form'>
-                    <form>
+                    <form ref={form} onSubmit={sendEmail}>
                         <ul>
                             <li className = "half">
-                                <input type="text" name="name" placeholder="Name" required/>
+                                <input type="text" name="user_name" placeholder="Name" required/>
                             </li>
                             <li className = "half">
-                                <input type="email" name="email" placeholder="Email" required/>
+                                <input type="email" name="user_email" placeholder="Email" required/>
                             </li>
                             <li>
                                 <input placeholder="Subject" type="text" name="subject" required/>
@@ -43,7 +70,9 @@ const Contact = () => {
                                 </textarea>
                             </li>
                             <li>
-                                <input type="submit" className='flat-button' value="SEND" />
+                                <input type="submit" className='flat-button' required>
+
+                                </input>
                             </li>
                         </ul>
                     </form>
