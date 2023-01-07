d1d4e4e8e8decc7dadda3c62ffa40fe526889921
ddavidhe <davidhe.3612@gmail.com>
2023-01-06 20:23:33 -0500

    irrelevant import removed

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,5 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.css'
-import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser, faEye} from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
