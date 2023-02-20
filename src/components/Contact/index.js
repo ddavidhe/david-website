516751ed159231dd32a9212a600c5c27bd9e6ed5
ddavidhe <davidhe.3612@gmail.com>
2023-02-20 10:59:54 -0500

    comment fix

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,10 +3,9 @@ import './index.css'
 import { useEffect, useRef, useState } from 'react'
 import emailjs from '@emailjs/browser';
 import Loader from 'react-loaders';
-{/*
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFile } from '@fortawesome/free-solid-svg-icons'
-*/}
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//import { faFile } from '@fortawesome/free-solid-svg-icons'
+
 
 function Contact() {
     const form = useRef();
