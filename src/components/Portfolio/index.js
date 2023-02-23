c1e0bd099e3c2b6976253e153e43a36f6e696bc7
ddavidhe <davidhe.3612@gmail.com>
2023-02-23 02:01:20 -0500

    working portfolio with sample images

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -24,7 +24,7 @@ function Portfolio() {
                             <div className="image-box" key={idx}>
                                 <img src="bunnymaria.png" className="portfolio-image" alt=""/>
                                 <div className="content">
-                                    <p className="title">{port.name}</p>
+                                    <p className="title">{port.title}</p>
                                     <h4 className="description">{port.description}</h4>
                                     <button
                                         className="btn"
