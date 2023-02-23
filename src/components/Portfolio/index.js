5fb163b8d97b05d56efea2afff42a255f3548876
ddavidhe <davidhe.3612@gmail.com>
2023-02-23 15:30:51 -0500

    portfolio responsivity complete

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -26,10 +26,7 @@ function Portfolio() {
                                 <div className="content">
                                     <p className="title">{port.title}</p>
                                     <h4 className="description">{port.description}</h4>
-                                    <button
-                                        className="btn"
-                                        onClick={() => window.open(port.url)}
-                                    >View</button>
+                                    <button className="btn" onClick={() => window.open(port.url)}>View</button>
                                 </div>
                             </div>
                         )
