90a678c31e89d4fe3040b51e0a123948e7193438
ddavidhe <davidhe.3612@gmail.com>
2023-02-22 23:34:55 -0500

    moved box to home, about mobile buttons work

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import Loader from 'react-loaders';
 import { Link } from 'react-router-dom';
 import AnimatedLetters from '../AnimatedLetters';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCss3Alt, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
 import './index.css';
 
 function Home() {
@@ -41,8 +43,32 @@ function Home() {
 
                 <h2> Mathematics student at UW, swimmer, board game enthusiast</h2>
                 <Link to="/contact" className="flat-button"> CONTACT ME </Link>
+            </div>   
+        </div>
+
+        <div className='stage-cube-cont'>
+            <div className='cubespinner'>
+                <div className='face1'>
+                    <img src="pythonlogo.png" alt="" className='pythonimage'/>
+                </div>
+                <div className='face2'>
+                        <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                </div>
+                <div className='face3'>
+                    <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                </div>
+                <div className='face4'>
+                    <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+                </div>
+                <div className='face5'>
+                    <FontAwesomeIcon icon={faCss3Alt} color="#28A4D9" />
+                </div>
+                <div className='face6'>
+                    <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                </div>
             </div>
         </div>
+
         <Loader type="ball-grid-pulse" />
         </>
 
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,8 +1,6 @@
 import './index.css';
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCss3Alt, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
 import Loader from 'react-loaders'
 
 function Portfolio() {
@@ -28,29 +26,6 @@ function Portfolio() {
                 </h1>
                 <p>Here are a few of my personal projects I've done! (STILL WORK IN PROGRESS)</p>
                 <p>I also have this neat animated cube which is showcasing my various skills</p>
-            </div>
-        </div>
-
-        <div className='stage-cube-cont'>
-            <div className='cubespinner'>
-                <div className='face1'>
-                    <img src="pythonlogo.png" alt="" className='pythonimage'/>
-                </div>
-                <div className='face2'>
-                        <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-                </div>
-                <div className='face3'>
-                    <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-                </div>
-                <div className='face4'>
-                    <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
-                </div>
-                <div className='face5'>
-                    <FontAwesomeIcon icon={faCss3Alt} color="#28A4D9" />
-                </div>
-                <div className='face6'>
-                    <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-                </div>
             </div>
         </div>
         <Loader type="ball-grid-pulse" />
